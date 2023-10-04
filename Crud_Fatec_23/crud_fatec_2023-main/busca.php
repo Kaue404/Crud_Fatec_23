bbd7f95aeaf27153cb51dda81cbc7d4b88655683
Kauê José <134783875+Kaue404@users.noreply.github.com>
2023-10-04 14:24:57 -0300

    Add files via upload

---
+++ Crud_Fatec_23/crud_fatec_2023-main/busca.php
@@ -0,0 +1,39 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+
+$connect = new PDO("mysql:host=localhost;dbname=id21333454_crud", "id21333454_kaue", "Kaueevinisius12#");
+
+$received_data = json_decode(file_get_contents("php://input"));
+
+$data = array();
+
+if($received_data->query != '')
+{
+	$query = "
+	SELECT * FROM fatec_alunos 
+	WHERE first_name LIKE '%".$received_data->query."%' 
+	OR last_name LIKE '%".$received_data->query."%' 
+	ORDER BY id DESC
+	";
+}
+else
+{
+	$query = "
+	SELECT * FROM fatec_alunos 
+	ORDER BY id DESC
+	";
+}
+
+$statement = $connect->prepare($query);
+
+$statement->execute();
+
+while($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+	$data[] = $row;
+}
+
+echo json_encode($data);
+
+?>
---
+++ Crud_Fatec_23/crud_fatec_2023-main/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = "localhost"; // nome do servidor MySQL
+$user = "id21333454_kaue"; // usuário do MySQL
+$pass = "Kaueevinisius12#"; // senha do MySQL
+$dbname = "id21333454_crud"; // nome do banco de dados
+
+// Conexão com o banco de dados MySQL
+$conn = mysqli_connect($host, $user, $pass, $dbname);
+
+// Verifica se houve erro na conexão
+if (!$conn) {
+    die("Falha na conexão: " . mysqli_connect_error());
+}
